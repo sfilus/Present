631d1bcbe0a70aa5f14b0f85e12e94ea73c08472
Stephen Filus <funkmasterfilus@gmail.com>
2020-01-21 06:23:17 -0500

    Configure webpack to load partials into page templates.

--- js/cover-letter.js
+++ js/cover-letter.js
@@ -1,2 +1,13 @@
 import Turbolinks from 'turbolinks';
+import { Application } from "stimulus";
+import { definitionsFromContext } from "stimulus/webpack-helpers";
+
 Turbolinks.start();
+
+const application = Application.start();
+const context = require.context("./controllers", true, /\.js$/);
+application.load(definitionsFromContext(context));
+
+//document.getElementById("right-arrow").onclick = function() {
+//  Turbolinks.visit("/why-i-want-to-work-at-basecamp/slide1.html")
+//};
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,26 @@
+const fs = require('fs');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const header = fs.readFileSync(__dirname + '/templates/partials/header.html');
+const footer = fs.readFileSync(__dirname + '/templates/partials/footer.html');
+const meta = fs.readFileSync(__dirname + '/templates/partials/meta.html');
+
 module.exports = {
+  mode: 'production',
   entry: './js/cover-letter.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'public/build')
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'templates/pages/1-intro.html'),
+      filename: path.resolve(__dirname, 'public/build/index.html'),
+      inject: false,
+      header: header,
+      footer: footer,
+      meta: meta
+    })
+  ]
 };
