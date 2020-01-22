caff44be5be5788814db0dca079de94b4aea8518
Stephen Filus <funkmasterfilus@gmail.com>
2020-01-22 07:02:24 -0500

    style changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        bcyellow: {
+          lighter: 'rgb(246, 242, 239)',
+          default: 'rgb(196, 182, 171)',
+          dark: 'rgb(179, 162, 148)'
+        }
+      }
+    }
   },
   variants: {},
   plugins: []
