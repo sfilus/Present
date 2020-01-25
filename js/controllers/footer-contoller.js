45f1f07717445bf51915eff07ed6b18429386244
Stephen Filus <funkmasterfilus@gmail.com>
2020-01-25 09:00:58 -0500

    Get history push state working.

--- js/controllers/footer-contoller.js
+++ js/controllers/footer-contoller.js
@@ -1,22 +1,34 @@
 import { Controller } from "stimulus";
+
 let pages = [
   'index.html',
   '2-why-i-want-to-work-at-basecamp.html'
 ];
 
+let domParser = new DOMParser();
+
+window.addEventListener('popstate', function(e) {
+  document.getElementById("content").innerHTML = e.state.content;
+});
+
 export default class extends Controller {
+
+  swapContent(html, pageIndex) {
+    var currentContentElement = document.getElementById("content");
+    var newContentElement = domParser.parseFromString(html, "text/html").getElementById("content");
+    currentContentElement.innerHTML = newContentElement.innerHTML;
+    window.history.pushState({content: newContentElement.innerHTML}, '', pages[pageIndex]);
+  }
 
   back() {
     fetch(pages[0])
       .then(response => response.text())
-      .then(html => this.element.innerHTML = html);
-      //todo: update browser history.
-      //todo: leave the footer permananet?
+      .then(html => this.swapContent(html, 0));
   }
 
   next() {
     fetch(pages[1])
       .then(response => response.text())
-      .then(html => this.element.innerHTML = html);
+      .then(html => this.swapContent(html, 1));
   }
 };
