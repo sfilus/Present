57c00f25facca21555b53f8d22b3e8295afea9ea
Stephen Filus <funkmasterfilus@gmail.com>
2020-01-28 07:40:44 -0500

    Grab the next/previous templates dynamically on the click events.

--- js/controllers/footer-contoller.js
+++ js/controllers/footer-contoller.js
@@ -1,21 +1,14 @@
 import { Controller } from "stimulus";
-
-let pages = [
-  'index.html',
-  '2-why-i-want-to-work-at-basecamp.html'
-];
-
 let domParser = new DOMParser();
 
 export default class extends Controller {
 
-  // static get targets() {
-  //   return ["contentBody"]
-  // }
+  static get targets() {
+    return ["slideList", "slideOption"]
+  }
 
   pickSlide(event) {
-    console.log(event);
-    console.log(event.target);
+    console.log("In pick slide");
     fetch(event.target.value)
       .then(response => response.text())
       .then(html => this.swapContent(html, 0));
@@ -28,15 +21,38 @@ export default class extends Controller {
     window.history.pushState({content: newContentElement.innerHTML}, '', templateName);
   }
 
+  getCurrentSlide() {
+    for(let i = 0; i < this.slideOptionTargets.length; i++) {
+      if(this.slideOptionTargets[i].selected) {
+        return {
+          slideOption: this.slideOptionTargets[i],
+          index: i
+        };
+      }
+    }
+  }
+
   back() {
-    fetch(pages[0])
+    let current = this.getCurrentSlide();
+    if(current.index === 0) {
+      console.log("You're already at the first page.");
+      return;
+    }
+    let previousTemplate = this.slideOptionTargets[current.index - 1].value;
+    fetch(previousTemplate)
       .then(response => response.text())
-      .then(html => this.swapContent(html, page[0]));
+      .then(html => this.swapContent(html, previousTemplate));
   }
 
   next() {
-    fetch(pages[1])
+    let current = this.getCurrentSlide();
+    if(current.index === this.slideOptionTargets.length -1) {
+      console.log("You're already at the end.");
+      return;
+    }
+    let nextTemplate = this.slideOptionTargets[current.index + 1].value;
+    fetch(nextTemplate)
       .then(response => response.text())
-      .then(html => this.swapContent(html, pages[1]));
+      .then(html => this.swapContent(html, nextTemplate))
   }
 };
