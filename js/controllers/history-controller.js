217160c947cf4a11a1d3b35c96cafb958c97b28c
Stephen Filus <funkmasterfilus@gmail.com>
2020-01-28 08:22:31 -0500

    begin history controller refactor.

--- js/controllers/history-controller.js
+++ js/controllers/history-controller.js
@@ -3,10 +3,12 @@ import { Controller } from "stimulus";
 export default class extends Controller {
 
   static get targets() {
-    return ["contentBody"]
+    return ["contentBody", "slideOption", "slideList"];
   }
 
   backState(event) {
+    console.log("options", this.slideOptionTargets);
+    console.log("select", this.slideListTarget);
     this.contentBodyTarget.outerHTML = event.state.content;
   }
 
--- js/cover-letter.js
+++ js/cover-letter.js
@@ -1,10 +1,10 @@
 import { Application } from "stimulus";
 import FooterController from "./controllers/footer-contoller";
-import ContentController from "./controllers/content-controller";
+import HistoryController from "./controllers/history-controller";
 
 const application = Application.start();
 
 addEventListener("DOMContentLoaded", () => {
   application.register("footer", FooterController);
-  application.register("content", ContentController);
+  application.register("history", HistoryController);
 });
