462501f22b2491ab7217d749cd6bbf8967a30b7e
Stephen Filus <funkmasterfilus@gmail.com>
2020-01-28 18:21:53 -0500

    Reorganize partials to make the easier to maintain.

--- webpack.config.js
+++ webpack.config.js
@@ -5,18 +5,16 @@ const _ = require('lodash');
 const ejs = require('ejs');
 
 const slidesJson = JSON.parse(fs.readFileSync(__dirname + '/slides.json', 'utf8'));
-
 const footerString = fs.readFileSync(__dirname + '/templates/partials/footer.ejs', 'utf8');
-let footer = ejs.render(footerString, {"pages": slidesJson.pages});
-
-const meta = fs.readFileSync(__dirname + '/templates/partials/meta.html');
+const footer = ejs.render(footerString, {"pages": slidesJson.pages});
+const header = fs.readFileSync(__dirname + '/templates/partials/header.html');
 
 let slidePlugins = _.map(slidesJson.pages, function(slide, index) {
   let config = {
       template: path.resolve(__dirname, 'templates/pages/', slide.template),
       filename: path.resolve(__dirname, 'public/build/', slide.buildTemplate),
       footer: footer,
-      meta: meta,
+      header: header,
       inject: false
   };
   return new HtmlWebpackPlugin(config);
