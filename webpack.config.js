2410701433c10c93af420f43def015449a05804c
Stephen Filus <funkmasterfilus@gmail.com>
2020-01-27 21:17:17 -0500

    Dynamically generate list of htmlwebpack plugins from the slides.json config file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
 const fs = require('fs');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const _ = require('lodash');
 
 const footer = fs.readFileSync(__dirname + '/templates/partials/footer.html');
 const meta = fs.readFileSync(__dirname + '/templates/partials/meta.html');
-
 const slidesJson = JSON.parse(fs.readFileSync(__dirname + '/slides.json', 'utf8'));
-console.log(slidesJson);
+
+let slidePlugins = _.map(slidesJson.pages, function(slide, index) {
+  let config = {
+      template: path.resolve(__dirname, 'templates/pages/', slide.template),
+      filename: path.resolve(__dirname, 'public/build/', slide.template),
+      footer: footer,
+      meta: meta,
+      inject: false
+  };
+  if(index === 0) {
+    config.filename = path.resolve(__dirname,'public/build/', 'index.html');
+  }
+  return new HtmlWebpackPlugin(config);
+});
 
 module.exports = {
   mode: 'production',
@@ -18,20 +31,5 @@ module.exports = {
   // loaders: [
   //   { test: /\.html$/, loader: "underscore-template-loader" }
   // ],
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'templates/pages/1-intro.html'),
-      filename: path.resolve(__dirname, 'public/build/index.html'),
-      inject: false,
-      footer: footer,
-      meta: meta
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'templates/pages/2-why-i-want-to-work-at-basecamp.html'),
-      filename: path.resolve(__dirname, 'public/build/2-why-i-want-to-work-at-basecamp.html'),
-      inject: false,
-      footer: footer,
-      meta: meta
-    })
-  ]
+  plugins: slidePlugins
 };
